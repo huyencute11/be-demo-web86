246a2c958a7114e5f78fdc7bdfdbbddc107e2c6d
Huyen Tran <ttminhuyen1111@gmail.com>
2025-02-06 19:55:06 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from 'dotenv';
+import connectDatabase from "./database/index.js";
+import RootRouter from "./routes/index.js";
+import cors from'cors';
+dotenv.config();
+
+const POST = process.env.PORT || 8080;
+app.use(cors());
+const app = express();
+app.use(express.json());
+
+connectDatabase();
+
+app.use('/api', RootRouter);
+app.get('', (req, res) => {
+    res.send('Hello mindx-er!');
+});
+
+app.listen(POST, () => {
+    console.log('Server is running!');
+});
